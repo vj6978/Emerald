918bc8a6bc913200d0c9e0aa7612d8e1699fa235
vj6978 <vj6978@gmail.com>
2019-12-28 12:08:47 +0000

    Upload file filepond dependency added

--- emerald/src/Components/NewPost.js
+++ emerald/src/Components/NewPost.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import { FilePond } from 'react-filepond';
+import 'filepond/dist/filepond.min.css';
 
 class NewPost extends Component
 {
@@ -29,6 +31,9 @@ class NewPost extends Component
                             </div>
         
                             <button type="submit" className="btn btn-primary">Submit</button>
+
+                            {/* Update server URL for file upload */}
+                            <FilePond allowMultiple={true} server="" />
                         </form>
                     </div>
                 </div>
