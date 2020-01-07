b3afe7bda1d1d85f01d38c743a411be19ba3b833
vj6978 <vj6978@gmail.com>
2020-01-07 22:26:27 +0000

    Added Issue List table

--- emerald/src/Components/IssueCard.js
+++ emerald/src/Components/IssueCard.js
@@ -1,41 +1,21 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './IssueCard.css'
 
-class IssueCard extends Component
+const IssueCard = (props) =>
 {
-    state = {
-
-    }
-
-    render() 
-    {
-        return(
-            <div className="issueCard">
-                <div className="card">
-                    <table className="table">
-                        <thead>
-                            <th scope="col">ID</th>
-                            <th scope="col">Issue</th>
-                            <th scope="col">Resolution</th>
-                            <th scope="col">Reported On</th>
-                            <th scope="col">Resolved On</th>
-                            <th scope="col">Comments</th>
-                            <th scope="col">Status</th>
-                        </thead>
-                        <tbody>
-                            <th scope="row">{props.ID}</th>
-                            <td>{props.issue}</td>
-                            <td>{props.resolution}</td>
-                            <td>{props.reportedOn}</td>
-                            <td>{props.resolvedOn}</td>
-                            <td>{props.comments}</td>
-                            <td>{props.status}</td>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        );
-    }
+    return(
+        <React.Fragment>
+            <tr>
+                <th scope="row">{props.issue.id}</th>
+                <td>{props.issue.issue}</td>
+                <td>{props.issue.resolution}</td>
+                <td>{props.issue.reportedOn}</td>
+                <td>{props.issue.resolvedOn}</td>
+                <td>{props.issue.comments}</td>
+                <td>{props.issue.status}</td>
+            </tr>
+        </React.Fragment>
+    );
 }
 
 export default IssueCard
--- emerald/src/Components/IssueList.js
+++ emerald/src/Components/IssueList.js
@@ -5,7 +5,54 @@ import './IssueList.css'
 class IssueList extends Component 
 {
     state = {
-        issues: []
+        issues: [
+            {
+                id: "1",
+                issue: "Pi Broke",
+                resolution: "Fixed it",
+                reportedOn: "10/10/10",
+                resolvedOn: "20/22/22",
+                comments: "Vimal JAmes",
+                status: "Closed"
+            },
+            {
+                id: "2",
+                issue: "Pi Broke",
+                resolution: "Fixed it",
+                reportedOn: "10/10/10",
+                resolvedOn: "20/22/22",
+                comments: "Vimal JAmes",
+                status: "Closed"
+            },
+            {
+                id: "3",
+                issue: "Pi Broke",
+                resolution: "Fixed it",
+                reportedOn: "10/10/10",
+                resolvedOn: "20/22/22",
+                comments: "Vimal JAmes",
+                status: "Closed"
+            },
+            {
+                id: "4",
+                issue: "Pi Broke",
+                resolution: "Fixed it",
+                reportedOn: "10/10/10",
+                resolvedOn: "20/22/22",
+                comments: "Vimal JAmes",
+                status: "Closed"
+            },
+            {
+                id: "5",
+                issue: "Pi Broke",
+                resolution: "Fixed it",
+                reportedOn: "10/10/10",
+                resolvedOn: "20/22/22",
+                comments: "Vimal JAmes",
+                status: "Closed"
+            },
+            
+        ]
     }
 
     componentDidMount = () => {
@@ -14,15 +61,30 @@ class IssueList extends Component
 
     render()
     {
+        let issueList = this.state.issues.length === 0 ? <div className="card"><h4>Nothing to show!</h4></div> : this.state.issues.map(issueItem => 
+            <IssueCard issue={issueItem} key={issueItem.id}></IssueCard>)
+
         return(
             <div>
-                {
-                    this.state.issues.length === 0 ? <div className="card"><h4>Nothing to show!</h4></div> : this.state.issues.map(issue =>
-                        <div>
-                            <IssueCard  issue={issue}></IssueCard> 
-                        </div>
-                    )
-                }
+                <div className="card">
+                    <h3 className="issueHeader">Issues We Faced So Far</h3>
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Issue</th>
+                                <th scope="col">Resolution</th>
+                                <th scope="col">Reported On</th>
+                                <th scope="col">Resolved On</th>
+                                <th scope="col">Comments</th>
+                                <th scope="col">Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {issueList}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
