fae2c0011079b501e879f22880cb4b8960b1e1ce
vj6978 <vj6978@gmail.com>
2020-01-09 20:32:25 +0000

    Working on comments feature

--- emerald/src/Components/CommentPane.js
+++ emerald/src/Components/CommentPane.js
@@ -9,27 +9,30 @@ class CommentPane extends Component
                 id: "1",
                 author: "Vimal",
                 date: "1-2-18",
-                body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+                body: "This is a good solution (Y)"
             },
             {
                 id: "2",
                 author: "Joseph",
                 date: "1-4-16",
-                body: "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+                body: "I doubt this would work but I guess we could give it a shot. Nonetheless I think it would make sense for us to have a fallback strategy/option."
             }]
     }
 
     render()
-    {
+    {        
         return(
-            <div>
-                <div className="commentPane">
-                    <div className="card">
-                        <div className="card-body">
-                            {/* Add logic for comments here */}
-                        </div>
-                    </div>
-                </div>
+            <div className="commentBlock">     
+                {
+                    this.state.comments.map(comment => 
+                                        <div className="comment" key={comment.id}>
+                                            <small><b>{comment.author}&nbsp;{comment.date}</b></small>
+                                            <div>
+                                                {comment.body}
+                                            </div>
+                                            <hr />
+                                        </div>)
+                }
             </div>
         );
     }
--- emerald/src/Components/Dashboard.js
+++ emerald/src/Components/Dashboard.js
@@ -15,7 +15,7 @@ class Dashboard extends Component
                 </div>
                 <div className="timeline-container">
                     <div className="issues-header">
-                        <a href="/issues" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Issues History</a>
+                        <a href="/issues" className="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Issues History</a>
                     </div>
                     <NewPost></NewPost>
                     <Timeline></Timeline>
