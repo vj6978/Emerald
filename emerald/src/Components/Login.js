553f17d4cbe8017eee32ef2d5e33faf4896e8168
vj6978 <vj6978@gmail.com>
2020-01-13 22:23:27 +0000

    Added Login Page

--- emerald/src/Components/Login.js
+++ emerald/src/Components/Login.js
@@ -7,11 +7,26 @@ class Login extends Component
 
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+        
+        // Handle User Login Here
+    }
+
     render()
     {
         return(
-            <div>
-                
+            <div className="login-component">
+                <div className="card">
+                    <img className="card-img-top" src="https://picsum.photos/id/237/200/300" alt="Login" />
+                    <div className="card-body">
+                        <div className="form-group">
+                            <input className="form-control" type="text" placeholder="Username"></input>
+                            <input className="form-control" type="password" placeholder="Password"></input>
+                            <button className="btn btn-primary" onClick={this.handleSubmit}>Login</button>
+                        </div> 
+                    </div>
+                </div>
             </div>
         );
     }
